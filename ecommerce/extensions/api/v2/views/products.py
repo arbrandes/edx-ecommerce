ab5305a0505465166f8bd75a72ad028b6da02414
vkaracic <karacicvedran@gmail.com>
2015-12-17 15:33:34 +0100

    Revert "Product filtering via query string"
    This reverts commit adedf319b1d5a763d8c31b3ee6b589916e546288.

--- ecommerce/extensions/api/v2/views/products.py
+++ ecommerce/extensions/api/v2/views/products.py
@@ -14,10 +14,3 @@ class ProductViewSet(NestedViewSetMixin, NonDestroyableModelViewSet):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductSerializer
     permission_classes = (IsAuthenticated, IsAdminUser,)
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        product_class_filter = self.request.query_params.get('product_class', None)
-        if product_class_filter:
-            queryset = Product.objects.filter(product_class__slug=product_class_filter.lower())
-        return queryset
