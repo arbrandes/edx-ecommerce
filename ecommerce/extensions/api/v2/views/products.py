8d92017bd1fafc1be70d1d011529479e26cde3a8
vkaracic <karacicvedran@gmail.com>
2015-12-17 16:23:52 +0100

    Include Product filtering via django-filters

--- ecommerce/extensions/api/v2/views/products.py
+++ ecommerce/extensions/api/v2/views/products.py
@@ -1,5 +1,6 @@
 """HTTP endpoints for interacting with products."""
 from oscar.core.loading import get_model
+from rest_framework import filters
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_extensions.mixins import NestedViewSetMixin
 
@@ -13,4 +14,9 @@ Product = get_model('catalogue', 'Product')
 class ProductViewSet(NestedViewSetMixin, NonDestroyableModelViewSet):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductSerializer
+    # URL with filter example:
+    # /api/v2/products/?product_class__name=Coupon
+    # Note, it's case-sensitive
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('product_class__name',)
     permission_classes = (IsAuthenticated, IsAdminUser,)
