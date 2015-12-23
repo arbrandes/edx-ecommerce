fabe9c0ec71f44007c392255b669bd840a266569
vkaracic <karacicvedran@gmail.com>
2015-12-23 17:24:48 +0100

    Add missing import

--- ecommerce/coupons/views.py
+++ ecommerce/coupons/views.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
